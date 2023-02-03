24f63e253e3f4050090de61c2c80444291cc20ab
Aleksei Elokhin <awiza@gmail.com>
2023-02-03 12:38:40 +0200

    all imports added

--- build.sbt
+++ build.sbt
@@ -4,8 +4,10 @@ ThisBuild / scalaVersion := "2.13.10"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "3.2.2" % "provided"
-  //"org.apache.spark" %% "spark-core" % "2.4.4" % "provided"
+//  "org.apache.spark" %% "spark-core" % "2.4.4" % "provided"
 )
+
+//scalacOptions += "-Ylog-classpath"
 
 lazy val root = (project in file("."))
   .settings(
--- src/main/scala/sparkstreaming/PrintTweets.scala
+++ src/main/scala/sparkstreaming/PrintTweets.scala
@@ -1,8 +1,16 @@
-
-
 package sparkstreaming
 
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter.TwitterUtils
 import sparkstreaming.Utilities.{setupLogging, setupTwitter}
+import StreamingContext._
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter._
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.log4j.Level
+import Utilities._
 
 /** Simple application to listen to a stream of Tweets and print them out */
 object PrintTweets {
@@ -22,10 +30,10 @@ object PrintTweets {
 
     // Create a DStream from Twitter using our streaming context
     val tweets = TwitterUtils.createStream(ssc, None)
-    
+
     // Now extract the text of each status update into RDD's using map()
     val statuses = tweets.map(status => status.getText())
-    
+
     // Print out the first ten
     statuses.print()
     
